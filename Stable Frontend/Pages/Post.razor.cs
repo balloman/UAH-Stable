4315d2cf348e7b1249118dfa605fafa984de9e62
aeruny <67850318+aeruny@users.noreply.github.com>
2021-03-27 12:45:21 -0500

    Reorganized NavMenu and Announcement pages. Added a Post page

---
+++ Stable Frontend/Pages/Post.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+
+namespace Stable_Frontend.Pages
+{
+    public partial class Post
+    {
+        public Post()
+        {
+
+        }
+    }
+}

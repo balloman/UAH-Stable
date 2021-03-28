678fcac3ff92e95f69e350965dc5839796056743
Bernard Allotey <benard.allotey4@gmail.com>
2021-03-28 10:55:22 -0500

    Added Petition Body

--- Stable Frontend/Pages/PetitionPost.razor.cs
+++ Stable Frontend/Pages/PetitionPost.razor.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Stable_Lib.Models;
 
 
 namespace Stable_Frontend.Pages
 {
     public partial class PetitionPost
     {
+        [Parameter]
+        public string Id { get; set; }
+        public string AuthorName { get; set; }
+        public Stable_Lib.Models.Petition Petition = new Stable_Lib.Models.Petition();
 
+        protected override async Task OnInitializedAsync()
+        {
+            var task = await Handler.GetPost("petitions", Id);
+            Petition = new Stable_Lib.Models.Petition(task.ToDictionary());
+            var task2 = await User.FromUid(Petition.Author);
+            AuthorName = task2.Name;
+            Console.WriteLine(Petition.ToString());
+        }
     }
 }

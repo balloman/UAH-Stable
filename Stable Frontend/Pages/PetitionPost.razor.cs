3c80f27788e1f5c6e98476092e41e82ec67dff21
aeruny <67850318+aeruny@users.noreply.github.com>
2021-03-28 11:58:21 -0500

    Added Voting Feature

--- Stable Frontend/Pages/PetitionPost.razor.cs
+++ Stable Frontend/Pages/PetitionPost.razor.cs
@@ -15,6 +15,8 @@ namespace Stable_Frontend.Pages
         public string Id { get; set; }
         public string AuthorName { get; set; }
         public Stable_Lib.Models.Petition Petition = new Stable_Lib.Models.Petition();
+        private int Votes;
+        private int counter = 0;
 
         protected override async Task OnInitializedAsync()
         {
@@ -22,7 +24,29 @@ namespace Stable_Frontend.Pages
             Petition = new Stable_Lib.Models.Petition(task.ToDictionary());
             var task2 = await User.FromUid(Petition.Author);
             AuthorName = task2.Name;
+            Votes = Petition.Votes;
             Console.WriteLine(Petition.ToString());
+            counter = 0;
+        }
+
+        
+        public void UpVote()
+        {
+            if (counter == 0)
+            {
+                Votes++;
+                Handler.SetPetitionVotes(Id, Votes);
+            }
+            counter++;
+        }
+        public void DownVote()
+        {
+            if (counter == 0)
+            {
+                Votes--;
+                Handler.SetPetitionVotes(Id, Votes);
+            }
+            counter++;
         }
     }
 }

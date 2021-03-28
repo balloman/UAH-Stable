e4646214feae816433d64c10dce1209006b1ed13
aeruny <67850318+aeruny@users.noreply.github.com>
2021-03-27 20:47:43 -0500

    Manipulated the WeatherForecast Classes to use them as example data for Petition Page

--- Stable Frontend/Data/WeatherForecast.cs
+++ Stable Frontend/Data/WeatherForecast.cs
@@ -6,10 +6,11 @@ namespace Stable_Frontend.Data
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public string Title { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public int Upvote { get; set; }
+        public int Downvote { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
     }
 }
--- Stable Frontend/Data/WeatherForecastService.cs
+++ Stable Frontend/Data/WeatherForecastService.cs
@@ -4,22 +4,34 @@ using System.Threading.Tasks;
 
 namespace Stable_Frontend.Data
 {
-    public class WeatherForecastService
-    {
-        private static readonly string[] Summaries = new[]
+    public class WeatherForecastService {
+        private static readonly string[] Categories = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Campus Environment", "Food", "Parking", "Food", "Campus Life"
+        };
+        
+        private static readonly string[] Names = new[]
+        {
+            "Parker Smith", "Mary Beeds", "James Henderson", "Riley Stuart", "William Roach", "Jesse Stummer", "Keira Balmy", "Eva Honduras", "Christopher Sworten", "Gena Reeves"
+        };
+        private static readonly string[] Titles = new[]
+        {
+            "Dirty Campus Maps", "Stop Requiring Meal Plans", "Parking Lot Access Is Too Limited For Students Living On Campus", "Open Campus Restaurants!", "COVID Restriction is too loose",
+            "Covid Policies Are Taking Our Freedom Away!"
         };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(0, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
+                Upvote = rng.Next(0, 60),
+                Downvote = rng.Next(0, 20),
+                Category = Categories[index],
+                Title = Titles[index],
+                Name = Names[rng.Next(0,Names.Length)],
+            }).ToArray()); 
         }
     }
 }
